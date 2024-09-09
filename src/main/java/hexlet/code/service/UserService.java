0f2ce22bfbd5e96dd35f921e57fe31f6b42589dc
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-09-09 16:19:31 +0300

    маппер

---
+++ src/main/java/hexlet/code/service/UserService.java
@@ -0,0 +1,85 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.user.UserCreateDTO;
+import hexlet.code.dto.user.UserUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.UserMapper;
+import hexlet.code.repository.UserRepository;
+import hexlet.code.dto.user.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<UserDTO> getAll() {
+        var users = userRepository.findAll();
+        return users.stream()
+                .map(userMapper::map)
+                .toList();
+    }
+
+    public UserDTO findById(Long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException ("User not found"));
+        return userMapper.map(user);
+    }
+
+    public UserDTO create(UserCreateDTO userData) {
+        var user = userMapper.map(userData);
+        var hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPasswordDigest(hashedPassword);
+
+        userRepository.save(user);
+
+        return userMapper.map(user);
+    }
+
+    public UserDTO update(UserUpdateDTO userData, Long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        userMapper.update(userData, user);
+
+        var hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPasswordDigest(hashedPassword);
+
+        return userMapper.map(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
+
+
+
+
+
+//@Service
+
+//
+
+//    public PostDTO update(PostUpdateDTO postData, Long id) {
+//        var post = repository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException("Not Found"));
+//        postMapper.update(postData, post);
+//        repository.save(post);
+//        var postDTO = postMapper.map(post);
+//        return postDTO;
+//    }
+//
+//    public void delete(Long id) {
+//        repository.deleteById(id);
+//    }
+//}
