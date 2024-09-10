2c3f727b8a420ac2557bc7233de14e80c91d60ec
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-09-10 10:04:31 +0300

    логирование

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
 	application
 	java
@@ -50,4 +53,11 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStackTraces = true
+		showCauses = true
+		showStandardStreams = true
+	}
 }
