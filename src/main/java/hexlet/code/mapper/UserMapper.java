f8c6e5baa3a0d0b319c317a305e6baa6e3e809ea
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-09-11 16:52:32 +0300

    ComponentModel

--- src/main/java/hexlet/code/mapper/UserMapper.java
+++ src/main/java/hexlet/code/mapper/UserMapper.java
@@ -7,9 +7,11 @@ import hexlet.code.model.User;
 import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,7 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Mapper(
         uses = { JsonNullableMapper.class, ReferenceMapper.class },
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-        componentModel = "spring",
+        componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class UserMapper {
@@ -38,16 +40,16 @@ public abstract class UserMapper {
         dto.setPassword(encoder.encode(password));
     }
 
-//    @BeforeMapping
-//    public void encryptPassword(UserUpdateDTO dto) {
-//        var password = dto.getPassword();
-//        if (password != null) {
-//            dto.setPassword(JsonNullable.of(encoder.encode(password.get())));
-//        }
-//    }
-//
-//    public void encryptPassword(User model) {
-//        var password = model.getPassword();
-//        model.setPasswordDigest(encoder.encode(password));
-//    }
+    @BeforeMapping
+    public void encryptPassword(UserUpdateDTO dto) {
+        var password = dto.getPassword();
+        if (password != null) {
+            dto.setPassword(JsonNullable.of(encoder.encode(password.get())));
+        }
+    }
+
+    public void encryptPassword(User model) {
+        var password = model.getPassword();
+        model.setPasswordDigest(encoder.encode(password));
+    }
 }
