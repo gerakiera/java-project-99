ac7e88761022e6996f30ee0fba842987e9217978
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-09-17 13:42:25 +0300

    set hashedPassword

--- src/main/java/hexlet/code/service/CustomUserDetailsService.java
+++ src/main/java/hexlet/code/service/CustomUserDetailsService.java
@@ -28,7 +28,9 @@ public class CustomUserDetailsService implements UserDetailsManager {
     public void createUser(UserDetails userData) {
         var user = new User();
         user.setEmail(userData.getUsername());
+
         var hashedPassword = passwordEncoder.encode(userData.getPassword());
+        user.setPasswordDigest(hashedPassword);
         userRepository.save(user);
     }
 
