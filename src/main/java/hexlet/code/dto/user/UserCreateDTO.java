5746dfcad533332c136d6ee9880bce036ff8baf7
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-09-17 13:35:53 +0300

    changed the annotations

--- src/main/java/hexlet/code/dto/user/UserCreateDTO.java
+++ src/main/java/hexlet/code/dto/user/UserCreateDTO.java
@@ -1,7 +1,7 @@
 package hexlet.code.dto.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserCreateDTO {
-    @NotNull
+    @NotBlank
     @Email
     private String email;
 
@@ -17,7 +17,7 @@ public class UserCreateDTO {
 
     private String lastName;
 
-    @NotNull
+    @NotBlank
     @Size(min = 3)
     private String password;
 }
