fe69023911c94d6f80f2f60802dc17df158c7df6
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-09-17 13:39:21 +0300

    task model

---
+++ src/main/java/hexlet/code/model/Task.java
@@ -0,0 +1,56 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tasks")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@ToString
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+
+    private Long index;
+
+    private String description;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    private TaskStatus taskStatus;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User assignee;
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Label> labels = new HashSet<>();
+}
